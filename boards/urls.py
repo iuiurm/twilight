bc6e4a04171d505aac66c9eee9a9fb0d33888254
edu-jason <kimsj1015@aol.com>
2019-06-10 11:49:40 +0900

    Create index, create pages

--- boards/urls.py
+++ boards/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),  # www.mulcam.com/boards/
+    path('new/', views.new),  # 사용자의 입력을 받아서 게시글을 작성하는 곳
+    path('create/', views.create),  # 사용자가 입력한 데이터를 전송받고 실제 DB에 작성 및 사용자 피드백
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from .models import Board
 
 
-# Create your views here.
+# Create your views here
 def index(request):
-    return render(request, 'boards/index.html')
+    # Board 의 전체 데이터를 불러온다 - QuerySet
+    boards = Board.objects.all()
+    context = {'boards': boards}
+    return render(request, 'boards/index.html', context)
+
+
+# 사용자 입력을 받는 페이지 렌더링
+def new(request):
+    return render(request, 'boards/new.html')
+
+
+# 데이터를 받아서 실제 DB 에 작성
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    board = Board(title=title, content=content)
+    board.save()
+    return render(request, 'boards/create.html')
