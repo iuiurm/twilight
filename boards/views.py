ab5514da0242ff506939d4f115a51094437516d7
edu-jason <kimsj1015@aol.com>
2019-06-10 17:05:13 +0900

    Finish CRD

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
 
 
@@ -17,8 +17,24 @@ def new(request):
 
 # 데이터를 받아서 실제 DB 에 작성
 def create(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
     board = Board(title=title, content=content)
     board.save()
-    return render(request, 'boards/create.html')
+    return redirect(f'/boards/{board.id}/')
+
+
+# 특정 게시글 하나만 가지고 온다.
+def detail(request, id):
+    # Board 클래스를 사용해서 id 값에 맞는 데이터를 가지고 온다.
+    # context 로 넘겨서 detail.html 페이지에서 title 과 content 를
+    # 출력해본다.
+    board = Board.objects.get(id=id)
+    context = {'board': board}
+    return render(request, 'boards/detail.html', context)
+
+
+def delete(request, id):
+    board = Board.objects.get(id=id)
+    board.delete()
+    return redirect('/boards/')
