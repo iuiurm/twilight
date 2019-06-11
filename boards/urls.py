8883671c5c96ca17fbfb8c360355930caf218c6b
edu-jason <kimsj1015@aol.com>
2019-06-11 10:24:30 +0900

    Finish update logic

--- boards/urls.py
+++ boards/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create/', views.create),  # 사용자가 입력한 데이터를 전송받고 실제 DB에 작성 및 사용자 피드백
     path('<int:id>/', views.detail),  # www.mulcam.com/boards/<id>/
     path('<int:id>/delete/', views.delete),
+    path('<int:id>/edit/', views.edit),  # 게시글 수정 페이지 렌더링
+    path('<int:id>/update/', views.update),  # 사용자가 입력한 수정 데이터를 전송받고 실제 DB 에 수정 후 저장
 ]
--- boards/views.py
+++ boards/views.py
@@ -38,3 +38,26 @@ def delete(request, id):
     board = Board.objects.get(id=id)
     board.delete()
     return redirect('/boards/')
+
+
+# 게시글 수정 페이지 렌더링
+def edit(request, id):
+    # id 값에 맞는 board 데이터 꺼낸 후 edit.html 로 넘기기
+    board = Board.objects.get(id=id)
+    context = {'board': board}
+    return render(request, 'boards/edit.html', context)
+
+
+def update(request, id):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # id 값에 맞는 board 데이터를 위에서 주어진 title 과 content 에 맞게
+    # 수정한 뒤 저장하는 로직
+    # 1. Board 클래스를 통해 id 값에 맞는 데이터를 가져온다.
+    board = Board.objects.get(id=id)
+    # 2. 해당 데이터의 내용을 주어진 title, content 로 수정한다.
+    board.title = title
+    board.content = content
+    # 3. 저장한다.
+    board.save()
+    return redirect(f'/boards/{id}/')
